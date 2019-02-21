a213b880b1397c4e5b588e73874c1ecf5c7f5f87
thechutrain <chu.chuyalan@gmail.com>
2019-02-21 14:51:32 -0500

    ignoring elm-stuff, changing the model to include list of teams

--- src/Main.elm
+++ src/Main.elm
@@ -1,43 +1,71 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
+
+
 -- MAIN
 
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Team =
+    { player_1 : String
+    , player_2 : String
+    }
 
 
 type alias Model =
-  { content : String
-  }
+    { teams : List Team
+    }
+
 
 init : Model
-init = 
-  { content = "hiiii" }
+init =
+    { teams = []
+    }
+
 
 
 -- UPDATE
 
 
-type Msg 
-  = Change String
+type Msg
+    = Add String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Change newContent ->
-      { model | content = newContent }
+    case msg of
+        Add str ->
+            let
+                test_team =
+                    { player_1 = str
+                    , player_2 = str
+                    }
+            in
+            { model | teams = test_team :: model.teams }
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div [] 
-    [ input [ placeholder model.content, onInput Change] []
-    , div [] [text model.content]
-    ]
+    div []
+        [ input [ placeholder "Yo imma placehold this", onInput Add ] []
+        ]
 
+
+
+-- createTeam : String -> Team
+-- createTeam str =
+--     { player_1 = str
+--     , player_2 = str
+--     }
